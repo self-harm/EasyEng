fcaa1f9b9cfd261d3ca8320338fd60376edc646f
Kseniia Levedeva <Kseniia.Lebedeva@dowjones.com>
2022-03-08 20:58:31 +0300

    init

---
+++ src/main/java/io/harmed/easyeng/EasyEngApplication.java
@@ -0,0 +1,13 @@
+package io.harmed.easyeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyEngApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EasyEngApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/harmed/easyeng/EasyEngApplicationTests.java
@@ -0,0 +1,13 @@
+package io.harmed.easyeng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyEngApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
